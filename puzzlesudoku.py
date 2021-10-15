b679e28fb94bc8b7a25d56e425f9fbba27b5001d
Nickhil1737 <58224547+Nickhil1737@users.noreply.github.com>
2021-10-15 12:32:21 +0530

    loads a sudoku puzzle to a file

--- puzzlesudoku.py
+++ puzzlesudoku.py
@@ -5,7 +5,6 @@ class CreateSudokuPuzzle:
     def __init__(self):
         perm = [i for i in range(1,9+1)]
         random.shuffle(perm)
-        print(perm)
         self.sudoku = [[0 for i in range(9)] for j in range(9)]
         k = -1
         for i in range(9):
@@ -15,19 +14,26 @@ class CreateSudokuPuzzle:
                 self.sudoku[i][j] = perm[k]
                 k = (k+1)%9
             k = (k+3)%9
-        for x in self.sudoku:
-            print(x)
 
         arrp = [i for i in range(81)]
         random.shuffle(arrp)
-        x = random.randint(16,20)
-        self.arrp = arrp[:x]
+        arrp = arrp[:random.randint(16,20)]
+        filled_set = set()
         self.filled_boxes = []
-        for x in self.arrp:
+        for x in arrp:
             r = x//9
             c = x%9
-            v = self.sudoku[r][c]
-            self.filled_boxes.append(str(r)+str(c)+str(v))
+            filled_set.add((r,c))
+        for i in range(9):
+            for j in range(9):
+                if (i,j) not in filled_set:
+                    self.sudoku[i][j] = 0
+        with open("sudoku_state_zero.txt","w") as fileob:
+            for i in range(9):
+                for j in range(9):
+                    fileob.write(str(self.sudoku[i][j]))
+                fileob.write('\n')
+
 
 # t1 = CreateSudokuPuzzle()
 # print(t1.filled_boxes)
